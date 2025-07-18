af75e5fa839adf13c1fc63939d860093c3f9647c
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-07-18 23:38:20 +0530

    tf aws tools jenkins

--- main.tf
+++ main.tf
@@ -10,13 +10,14 @@ module "jenkins_master" {
   user_data               = file("${path.module}/install_jenkins_master.sh")
 
   # Define the root volume size and type
-  root_block_device = [
-    {
-      volume_size = 50       # Size of the root volume in GB
-      volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
-      delete_on_termination = true  # Automatically delete the volume when the instance is terminated
-    }
-  ]
+  root_block_device {
+    encrypted             = false
+    volume_type           = "gp3"
+    volume_size           = 50
+    iops                  = 3000
+    throughput            = 125
+    delete_on_termination = true
+  }
 
   tags = {
     Name   = "Jenkins-Master"
@@ -34,13 +35,14 @@ module "jenkins_agent" {
   user_data               = file("${path.module}/install_jenkins_agent.sh")
 
    # Define the root volume size and type
-  root_block_device = [
-    {
-      volume_size = 50       # Size of the root volume in GB
-      volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
-      delete_on_termination = true  # Automatically delete the volume when the instance is terminated
-    }
-  ]
+  root_block_device {
+    encrypted             = false
+    volume_type           = "gp3"
+    volume_size           = 50
+    iops                  = 3000
+    throughput            = 100
+    delete_on_termination = true
+  }
 
   tags = {
     Name   = "Jenkins-Agent"
@@ -56,50 +58,50 @@ resource "aws_key_pair" "tools" {
 
 }
 
-# module "nexus" {
-#   source  = "terraform-aws-modules/ec2-instance/aws"
-#   name = "nexus"
-#   instance_type          = "t3.medium"
-#   vpc_security_group_ids = [var.allow_everything] #replace your SG
-#   ami                    = data.aws_ami.nexus_ami_info.id
-#   key_name               = aws_key_pair.tools.key_name
+module "nexus" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  name = "nexus"
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = [var.allow_everything] #replace your SG
+  ami                    = data.aws_ami.nexus_ami_info.id
+  key_name               = aws_key_pair.tools.key_name
    
-#   # Define the root volume size and type
-#   root_block_device = [
-#     {
-#       volume_size = 30       # Size of the root volume in GB
-#       volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
-#       delete_on_termination = true  # Automatically delete the volume when the instance is terminated
-#     }
-#   ]
+  # Define the root volume size and type
+  root_block_device = [
+    {
+      volume_size = 30       # Size of the root volume in GB
+      volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
+      delete_on_termination = true  # Automatically delete the volume when the instance is terminated
+    }
+  ]
   
-#   tags = {
-#     Name   = "Nexus"
-#   }
-# }
-# module "sonarqube" {
-#   source  = "terraform-aws-modules/ec2-instance/aws"
-#   name = "sonarqube"
+  tags = {
+    Name   = "Nexus"
+  }
+}
+module "sonarqube" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  name = "sonarqube"
 
-#   instance_type          = "t3.medium"
-#   vpc_security_group_ids = [var.allow_everything] #replace your SG
-#   ami                   = data.aws_ami.sonarqube_ami_info.id
-#   #ami                   = "ami-0649f08ef033b0cc2"
-#   key_name = aws_key_pair.tools.key_name
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = [var.allow_everything] #replace your SG
+  ami                   = data.aws_ami.sonarqube_ami_info.id
+  #ami                   = "ami-0649f08ef033b0cc2"
+  key_name = aws_key_pair.tools.key_name
    
-#   # Define the root volume size and type
-#   root_block_device = [
-#     {
-#       volume_size = 50       # Size of the root volume in GB
-#       volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
-#       delete_on_termination = true  # Automatically delete the volume when the instance is terminated
-#     }
-#   ]
+  # Define the root volume size and type
+  root_block_device = [
+    {
+      volume_size = 50       # Size of the root volume in GB
+      volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
+      delete_on_termination = true  # Automatically delete the volume when the instance is terminated
+    }
+  ]
   
-#   tags = {
-#     Name   = "SonarQube"
-#   }
-# }
+  tags = {
+    Name   = "SonarQube"
+  }
+}
 
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
@@ -126,24 +128,24 @@ records = [
         ]
         allow_overwrite = true
         }
-      #   ,
-      #   {
-      #   name = "nexus"
-      #   type = "A"
-      #   ttl  = 1
-      #   records = [
-      #     module.nexus.public_ip
-      #   ]
-      #   allow_overwrite = true
-      # } ,
-      # {
-      #   name = "sonarqube"
-      #   type = "A"
-      #   ttl  = 1
-      #   records = [
-      #     module.sonarqube.public_ip
-      #   ]
-      #   allow_overwrite = true
-      # }
+        ,
+        {
+        name = "nexus"
+        type = "A"
+        ttl  = 1
+        records = [
+          module.nexus.public_ip
+        ]
+        allow_overwrite = true
+      } ,
+      {
+        name = "sonarqube"
+        type = "A"
+        ttl  = 1
+        records = [
+          module.sonarqube.public_ip
+        ]
+        allow_overwrite = true
+      }
    ]
 }
